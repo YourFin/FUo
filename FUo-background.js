61d724bb44a9769985aea66733c4a7459f1f8d66
YourFin <d.junkpobox+git@gmail.com>
2019-12-13 20:19:50 -0800

    Works on firefox

--- FUo-background.js
+++ FUo-background.js
@@ -1,8 +1,18 @@
+"use strict";
+
+// For cross browser compatibility
+window.browser = (function () {
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome;
+})();
+
 var duo_valid_user_agent =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/10000.0.0 Safari/537.36';
 
 
 function rewriteUserAgentHeader(e) {
+    console.log("Rewriting...");
     e.requestHeaders.forEach(function(header){
         if (header.name.toLowerCase() == "user-agent") {
             header.value = duo_valid_user_agent;
@@ -11,7 +21,8 @@ function rewriteUserAgentHeader(e) {
     return {requestHeaders: e.requestHeaders};
 }
 
-var targetPages = ["*://*.duosecurity.com/*"];
+var targetPages = ["http://*.duosecurity.com/*",
+                   "https://*.duosecurity.com/*"];
 browser.webRequest.onBeforeSendHeaders.addListener(
     rewriteUserAgentHeader,
     {urls: targetPages},
