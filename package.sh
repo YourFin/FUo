7f4b867dbfe202a15eebc3a98cf4998e225992c7
YourFin <d.junkpobox+git@gmail.com>
2019-12-13 23:14:05 -0800

    Add build script

---
+++ package.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Unofficial Strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# cd into script directory
+cleanup() {
+    popd &>/dev/null || true
+}
+trap cleanup EXIT
+pushd "$SCRIPTPATH" >/dev/null
+
+
+# Generate logos
+mkdir -p "./public/icons"
+EXTENSION_SIZES=(16 48 128)
+for size in ${EXTENSION_SIZES[@]} ; do
+    echo
+    echo "Build icon of size ${size}..."
+    echo
+    inkscape -z -e "public/icons/fuo-${size}.png" -w $size -h $size ./logo.svg
+done
+
+rm -f fuo.zip
+
+cleanupPrime() {
+    popd &>/dev/null || true
+    popd &>/dev/null || true
+}
+trap cleanupPrime EXIT
+pushd "public" >/dev/null
+
+zip -r -FS ../fuo.zip *
